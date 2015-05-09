ece2f41babc8b540fc5dfdef7cf3fe3753a54285
sinsys1981 <sinsys@gmail.com>
2015-05-09 13:59:31 -0230

    Added inventory
    Added inventory script to Graham and gave Star script ability to add to
    inventory.

---
+++ Assets/Inventory.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+
+var hasStar : boolean;
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function getStar() {
+	hasStar = true;
+	Debug.Log("Has Star");
+}
+
+function loseStar() {
+	hasStar = false;
+	Debug.Log("Lost Star");
+}
