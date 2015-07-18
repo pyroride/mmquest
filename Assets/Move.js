bb8cb79b011f2dfa9555fcaf7d20a11fb3bf0723
rob-norris-vgh <rob.norris@verafin.com>
2015-07-18 13:14:12 -0230

    Upgraded to Unity 5.0

--- Assets/Move.js
+++ Assets/Move.js
@@ -18,28 +18,28 @@ function FixedUpdate ()
 {
 	if (Input.GetKey(moveUp))
 	{
-		rigidbody2D.velocity.y = speed;
+		GetComponent.<Rigidbody2D>().velocity.y = speed;
 		grahamAnimator.SetFloat("moveVert", speed);
 	}
 	else if (Input.GetKey(moveDown))
 	{
-		rigidbody2D.velocity.y = speed *-1;
+		GetComponent.<Rigidbody2D>().velocity.y = speed *-1;
 		grahamAnimator.SetFloat("moveVert", speed *-1);
 	}
 	else if (Input.GetKey(moveLeft))
 	{
-		rigidbody2D.velocity.x = speed *-1;
+		GetComponent.<Rigidbody2D>().velocity.x = speed *-1;
 		grahamAnimator.SetFloat("moveHor", speed *-1);
 	}
 	else if (Input.GetKey(moveRight))
 	{
-		rigidbody2D.velocity.x = speed;
+		GetComponent.<Rigidbody2D>().velocity.x = speed;
 		grahamAnimator.SetFloat("moveHor", speed);
 	}
 	else
 	{
-		rigidbody2D.velocity.y = 0;
-		rigidbody2D.velocity.x = 0;
+		GetComponent.<Rigidbody2D>().velocity.y = 0;
+		GetComponent.<Rigidbody2D>().velocity.x = 0;
 		grahamAnimator.SetFloat("moveHor", 0);
 		grahamAnimator.SetFloat("moveVert", 0);
 	}
